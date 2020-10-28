736ad60054139f4cf8505900e45e180f7fec7790
Gus2730 <gus2730@gmail.com>
2020-10-27 18:11:02 -0600

    vista menu cobros

---
+++ examenP3Cliente/src/main/java/org/una/examenp3cliente/controllers/MenuCobrosPendientesController.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.examenp3cliente.controllers;
+
+import com.jfoenix.controls.JFXButton;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author cfugu
+ */
+public class MenuCobrosPendientesController implements Initializable {
+
+    @FXML
+    private JFXButton btnVisualizar;
+    @FXML
+    private JFXButton btnGenerar;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+
+    @FXML
+    private void actionGenerarCobros(ActionEvent event) {
+    }
+
+    @FXML
+    private void actionVisualizarCobros(ActionEvent event) {
+    }
+    
+}
